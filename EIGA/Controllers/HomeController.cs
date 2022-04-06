57cffb23e3af15cb57c49454bc7c5ec95021a824
jesse tjiook <110632@st-maartenscollege.nl>
2022-04-06 12:52:53 +0200

    Edited Posters

--- EIGA/Controllers/HomeController.cs
+++ EIGA/Controllers/HomeController.cs
@@ -10,73 +10,74 @@ using System.Threading.Tasks;
 
 namespace EIGA.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-      _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-      var names = GetNames();
-
-      return View(names);
-    }
-
-    public List<Film> GetNames()
-    {
-      // stel in waar de database gevonden kan worden
-      string connectionString = "Server=172.16.160.21;Port=3306;Database=110632;Uid=110632;Pwd=inf2122sql;";
-
-      // maak een lege lijst waar we de namen in gaan opslaan
-      List<Film> Film = new List<Film>();
-
-      // verbinding maken met de database
-      using (MySqlConnection conn = new MySqlConnection(connectionString))
-      {
-        // verbinding openen
-        conn.Open();
-
-        // SQL query die we willen uitvoeren
-        MySqlCommand cmd = new MySqlCommand("select * from film", conn);
-
-        // resultaat van de query lezen
-        using (var reader = cmd.ExecuteReader())
+        public HomeController(ILogger<HomeController> logger)
         {
-          // elke keer een regel (of eigenlijk: database rij) lezen
-          while (reader.Read())
-          {
-            // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
-            string Name = reader["Naam"].ToString();
-            string Poster = reader["Poster"].ToString();
-
-            Film p = new Film();
-            // voeg de naam toe aan de lijst met namen
-            p.Name = Name;
-            p.Poster = Poster;
-
-            Film.Add(p);
-
-          }
+            _logger = logger;
         }
-      }
 
-      // return de lijst met namen
-      return Film;
-    }
+        public IActionResult Index()
+        {
+            var names = GetNames();
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
+            return View(names);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public List<Film> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110632;Uid=110632;Pwd=inf2122sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Film> Film = new List<Film>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from film", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+                        string Poster = reader["Poster"].ToString();
+
+                        Film p = new Film();
+                        // voeg de naam toe aan de lijst met namen
+                        p.Id = Convert.ToInt32(reader["id"].ToString());
+                        p.Name = Name;
+                        p.Poster = Poster;
+
+                        Film.Add(p);
+
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return Film;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-  }
 }
--- EIGA/Models/Product.cs
+++ EIGA/Models/Product.cs
@@ -2,7 +2,8 @@
 {
   public class Film
   {
-    public string Name { get; set; } 
+    public int Id { get; set; }
+        public string Name { get; set; } 
 
     public string Poster { get; set; } 
 
