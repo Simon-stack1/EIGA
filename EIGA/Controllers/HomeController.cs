02c7ab374fce3262322c220092c10c0a411085f9
jesse tjiook <110632@st-maartenscollege.nl>
2022-04-11 16:03:03 +0200

    FilmDetail pages added + logo reference to home page works

--- EIGA/Controllers/HomeController.cs
+++ EIGA/Controllers/HomeController.cs
@@ -21,12 +21,63 @@ namespace EIGA.Controllers
 
         public IActionResult Index()
         {
-            var names = GetNames();
+            var names = GetAllMovies();
 
             return View(names);
         }
 
-        public List<Film> GetNames()
+        [Route("film/{id}")]
+        public IActionResult FilmDetail(int id)
+        {
+            var film = GetMovieDetails(id);
+            return View(film);
+        }
+
+        public Film GetMovieDetails(int movieId)
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=110632;Uid=110632;Pwd=inf2122sql;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<Film> films = new List<Film>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand($"select * from film where id = {movieId}", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+                        string Poster = reader["Poster"].ToString();
+
+                        Film p = new Film();
+                        // voeg de naam toe aan de lijst met namen
+                        p.Id = Convert.ToInt32(reader["id"].ToString());
+                        p.Name = Name;
+                        p.Poster = Poster;
+
+                        films.Add(p);
+
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return films[0];
+        }
+
+
+        public List<Film> GetAllMovies()
         {
             // stel in waar de database gevonden kan worden
             string connectionString = "Server=172.16.160.21;Port=3306;Database=110632;Uid=110632;Pwd=inf2122sql;";
---
+++ EIGA/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace EIGA.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Poster { get; set; }
+
+    }
+}
